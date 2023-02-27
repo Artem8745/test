7712d14371dad4e61879bc44e9eba6d09f153456
Artem8745 <102891549+Artem8745@users.noreply.github.com>
2023-02-27 20:55:34 +0400

    add

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const $button = document.querySelector('ab')
-const $form = document.querySelector('about_me-1')
+const rus = document.querySelector('.rus')
+const ru = document.querySelectorAll('.ru')
+const eng = document.querySelector('.eng')
+const en = document.querySelectorAll('.en')
 
-$button.addEventListener('click', e => {
-    // Прокрутим страницу к форме 
-    $form.scrollIntoView({ 
-      block: 'nearest', // к ближайшей границе экрана
-      behavior: 'smooth', // и плавно 
-    });
-  });
+rus.addEventListener('click', () => {
+    for(var i=0; i<en.length; i++)en[i].style.display='none'
+    for(var i=0; i<ru.length; i++)ru[i].style.display='flex'
+})
+
+eng.addEventListener('click', () => {
+    for(var i=0; i<en.length; i++)en[i].style.display='flex'
+    for(let i = 0; i < ru.length; i++)ru[i].style.display='none'
+})
+
+defolt = () => {
+    for(var i=0; i<en.length; i++)en[i].style.display='flex'
+    for(let i = 0; i < ru.length; i++)ru[i].style.display='none'
+}
+
+defolt()
