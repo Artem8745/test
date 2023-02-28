33c4ccadf77cc42af16e3cb484f8ec926fd00d5d
Artem8745 <102891549+Artem8745@users.noreply.github.com>
2023-02-28 06:29:14 +0400

    add

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-const rus = document.querySelector('.rus')
+const rus = document.querySelectorAll('.rus')
 const ru = document.querySelectorAll('.ru')
-const eng = document.querySelector('.eng')
+const eng = document.querySelectorAll('.eng')
 const en = document.querySelectorAll('.en')
-
-rus.addEventListener('click', () => {
-    for(var i=0; i<en.length; i++)en[i].style.display='none'
-    for(var i=0; i<ru.length; i++)ru[i].style.display='flex'
-})
-
-eng.addEventListener('click', () => {
-    for(var i=0; i<en.length; i++)en[i].style.display='flex'
-    for(let i = 0; i < ru.length; i++)ru[i].style.display='none'
-})
 
 defolt = () => {
     for(var i=0; i<en.length; i++)en[i].style.display='flex'
@@ -19,3 +9,17 @@ defolt = () => {
 }
 
 defolt()
+
+const ruse = [].forEach.call(rus, function(el){
+    el.addEventListener('click', () => {
+        for(var i=0; i<en.length; i++)en[i].style.display='none'
+    for(var i=0; i<ru.length; i++)ru[i].style.display='flex'
+    })
+});
+
+const enge = [].forEach.call(eng, function(el){
+    el.addEventListener('click', () => {
+        for(var i=0; i<en.length; i++)en[i].style.display='flex'
+    for(let i = 0; i < ru.length; i++)ru[i].style.display='none'
+    })
+})
