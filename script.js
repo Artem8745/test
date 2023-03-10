656a81f237076b764f60c4badb502abf57379070
Artem8745 <102891549+Artem8745@users.noreply.github.com>
2023-03-10 18:09:46 +0400

    add

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ru = document.querySelectorAll('.ru')
 const eng = document.querySelectorAll('.eng')
 const en = document.querySelectorAll('.en')
 const ab = document.querySelector('.ab')
+const body = document.body
 
 defolt = () => {
     for(var i=0; i<en.length; i++)en[i].style.display='flex'
@@ -28,3 +29,47 @@ const enge = [].forEach.call(eng, function(el){
         for(let i=0; i<eng.length; i++)eng[i].style.color = 'black'
     })
 })
+
+
+
+// Прокрутка при клике 
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target
+        // Проверка ошибок
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto)
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight // Учитывает расстояние шапки
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            })
+            e.preventDefault()
+        }
+    }
+}
+
+window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+  
+    if (window.innerWidth > 768) {
+      document.querySelectorAll('.section').forEach((el, i) => {
+        if (el.offsetTop - document.querySelector('.navbar').clientHeight <= scrollDistance) {
+          document.querySelectorAll('.navbar a').forEach((el) => {
+            if (el.classList.contains('active')) {
+              el.classList.remove('active');
+            }
+          });
+  
+          document.querySelectorAll('.navbar li')[i].querySelector('a').classList.add('active');
+        }
+      });
+    }
+  });
